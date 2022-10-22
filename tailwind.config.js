9a79e29597a8e00ec2e95eaaf16ce13e45dfb182
722sofiaameh <sofiagabriel722@gmail.com>
2022-10-22 15:56:16 -0700

    add desktop design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["index.html"],
   theme: {
     extend: {
       colors:{
         textColor : "hsl(0, 0%, 55%)",
         headingColor : "hsl(0, 0%, 41%)"
+      },
+      screens:{
+        sm: '200px',
+        md: '700px'
       }
     }
   },
